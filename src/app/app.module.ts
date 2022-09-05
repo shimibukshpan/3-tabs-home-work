877519ce2ba825186ace49af77ef0840e312296e
shimi bukshpan <buki8060@gmail.com>
2022-09-05 13:30:45 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainComponent } from './main/main.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { Tab1Component } from './tab1/tab1.component';
+import { Tab2Component } from './tab2/tab2.component';
+import { Tab3Component } from './tab3/tab3.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    TabsComponent,
+    Tab1Component,
+    Tab2Component,
+    Tab3Component
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
